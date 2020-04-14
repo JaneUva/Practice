a5a29c1f79a7fc965b541a01b929833c86cf2a34
Jana Uvarova <uvarova.jana@gmail.com>
2020-04-14 19:34:00 +0200

    updated code to ES6

--- js/01/script.js
+++ js/01/script.js
@@ -1,47 +1,47 @@
-var button = document.getElementById('enter');
-var input = document.getElementById('userinput');
-var unorderedlist = document.querySelector('ul');
+const button = document.getElementById('enter');
+const input = document.getElementById('userinput');
+const unorderedlist = document.querySelector('ul');
 
-function inputLength() {
+const inputLength = () => {
     return input.value.length;
-}
+};
 
-function createListElement() {
-    var li = document.createElement('li');
+const createListElement = () => {
+    const li = document.createElement('li');
     li.appendChild(document.createTextNode(input.value));
     decorateListItem(li);
     unorderedlist.appendChild(li);
     input.value = '';
-}
+};
 
-function addListAfterClick() {
+const addListAfterClick = () => {
     if (inputLength() > 0) {
         createListElement();
     }
-}
+};
 
-function addListAfterKeyPress(event) {
+const addListAfterKeyPress = (event) => {
     if (inputLength() > 0 && event.keyCode === 13) {
         createListElement();
     }
-}
+};
 
-function decorateListItem(listItem) {
-    var deleteButton = document.createElement('button');
+const decorateListItem = (listItem) => {
+    const deleteButton = document.createElement('button');
     deleteButton.innerText = "delete";
-    deleteButton.addEventListener('click', function () {
+    deleteButton.addEventListener('click', () => {
         unorderedlist.removeChild(listItem);
     });
     listItem.addEventListener('click', function () {
         this.classList.toggle('done');
     });
     listItem.appendChild(deleteButton);
-}
+};
 
 
 const listAsArray = Array.from(unorderedlist.querySelectorAll('li'));
 
-listAsArray.forEach(node => {
+listAsArray.forEach((node) => {
     decorateListItem(node);
 });
 
